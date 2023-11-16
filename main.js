f892163308eeb268b83ef351b14f9420e4253267
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2023-11-16 11:14:08 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let input=document.querySelector('#input');
 //local storage
 /* const localStorageKey="groupProject"
 let todoList=JSON.parse(localStorage.getItem(localStorageKey))  ||  [] */
-
+localStorage.setItem('todoList',JSON.stringify)
 
 //create a todo array helps to add and delete from storage easily
 let todoList=[];
@@ -31,5 +31,5 @@ function addlist(list){
        remove(list)
 
     })
-    localStorage.setItem('todoList',JSON.stringify)
+   
 }
