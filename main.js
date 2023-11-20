344f485785fe60c40896c8226d28dd3fb4172305
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2023-11-20 15:01:26 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,21 +2,24 @@ let todo=document.querySelector('#todo');
 let button=document.querySelector('#add');
 let input=document.querySelector('#input');
 
+
+
 //local storage
 /* const localStorageKey="groupProject"
 let todoList=JSON.parse(localStorage.getItem(localStorageKey))  ||  [] */
-localStorage.setItem('todoList',JSON.stringify)
+
 
 //create a todo array helps to add and delete from storage easily
 let todoList=[];
-
-
-
+window.onload=()=>{
+   todoList= JSON.parse(localStorage.getItem('todoList')) || []
+   todoList.forEach(todoList=>addlist(todoList))
+}
 //add eventlistener for button element
 
 button.addEventListener('click',()=>{
     todoList.push(input.value);
-    addlist(input.value);
+   addlist(input.value);
     input.value='';
 })
 
@@ -26,9 +29,10 @@ function addlist(list){
     let para = document.createElement("p");
     para.innerText=list;
     todo.appendChild(para);
+    localStorage.setItem('todoList',JSON.stringify(todoList))
     para.addEventListener('dblclick',()=>{
        todo.removeChild(para)
-       remove(list)
+       
 
     })
    
