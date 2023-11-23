655106280c399a3d7efb088f5e22fc9f752ee05d
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2023-11-23 12:57:12 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,19 +2,13 @@ let todo=document.querySelector('#todo');
 let button=document.querySelector('#add');
 let input=document.querySelector('#input');
 
-
-
-//local storage
-/* const localStorageKey="groupProject"
-let todoList=JSON.parse(localStorage.getItem(localStorageKey))  ||  [] */
-
-
-//create a todo array helps to add and delete from storage easily
+//create a todoList array helps to add and delete from storage easily
 let todoList=[];
 window.onload=()=>{
    todoList= JSON.parse(localStorage.getItem('todoList')) || []
-   todoList.forEach(todoList=>addlist(todoList))
+   todoList.forEach(list=>addlist(list))
 }
+console.log(todo)
 //add eventlistener for button element
 
 button.addEventListener('click',()=>{
@@ -22,18 +16,48 @@ button.addEventListener('click',()=>{
    addlist(input.value);
     input.value='';
 })
-
+ 
 
 function addlist(list){
     //creating p elements dynamically
     let para = document.createElement("p");
     para.innerText=list;
+   
     todo.appendChild(para);
     localStorage.setItem('todoList',JSON.stringify(todoList))
     para.addEventListener('dblclick',()=>{
-       todo.removeChild(para)
-       
+       todo.removeChild(para);
+       remove(list);
 
     })
+    function remove(list){
+      let index = todoList.indexOf(list)
+      if (index > -1) {
+       
+          todoList.splice(index, 1);
+          
+        }
+        
+      localStorage.setItem('todoList',JSON.stringify(todoList))
+      
+  }
    
 }
+
+/* const makeElement = (type, properties) => {
+    const element = document.createElement(type)
+    // convert the properties object into a (key-value pair) array: 
+    const propertyPairs = Object.entries(properties)
+    
+    propertyPairs.forEach(property => {
+      // destructure the property array:
+      const [propertyKey, propertyValue] = property
+  
+      element[propertyKey] = propertyValue
+    })
+  
+    return element
+  }  */
+  /*const helloBtn = makeElement("button", {className: "helloBtn", textContent: "hello!", id: "btnId"});
+  document.body.append(helloBtn);*/
+ 
